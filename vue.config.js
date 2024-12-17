71f96b795bca07312eac74560cd929607eca6f80
Devaprab <devaprabha02@gmail.com>
2024-12-17 12:27:57 +0530

    env file created

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
  devServer: {
-        allowedHosts:"all"
+        allowedHosts:"all",
+        port: process.env.VUE_APP_PORT || 8080, 
   },
  publicPath: '/admin-spot/',
   pluginOptions: {
