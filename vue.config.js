277ec02e2e065c1eac145e221af3e264322a0d23
Devaprab <devaprabha02@gmail.com>
2025-01-30 22:03:26 +0530

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-//  devServer: {
-//         allowedHosts:"all",
-//         port: process.env.VUE_APP_PORT || 8080, 
-//   },
+ devServer: {
+        allowedHosts:"all",
+        // port: process.env.VUE_APP_PORT || 8080, 
+  },
  publicPath: '/admin-spot/',
   pluginOptions: {
     vuetify: {
